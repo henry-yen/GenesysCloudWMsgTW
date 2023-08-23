45e754977e64478e815f719fd0846d079b21b945
Henry Yen <henryyen0626@gmail.com>
2023-08-23 17:29:17 +0800

    Test

--- script.js
+++ script.js
@@ -12,12 +12,12 @@
     deploymentId: '55b977a6-5a9a-4548-b2bb-f7485763eb14',
     lang: 'zh-TW',
     
-  });
-  window._genesys.widgets.cobrowse = {
+  },window._genesys.widgets.cobrowse = {
     src: 'https://apps.mypurecloud.jp/genesys-bootstrap/genesys.min.js'+'/cobrowse/js/gcb.min.js?apikey=55b977a6-5a9a-4548-b2bb-f7485763eb14',
     url: 'https://apps.mypurecloud.jp/genesys-bootstrap/genesys.min.js'+'/cobrowse/',
     apikey: '55b977a6-5a9a-4548-b2bb-f7485763eb14',
     lang: 'zh-TW'
-    };
+    });
+  
 
   
