da1674265be9af02f35f3eb82fe74df75ba56963
Henry Yen <henryyen0626@gmail.com>
2023-08-23 17:25:38 +0800

    Test

--- script.js
+++ script.js
@@ -13,7 +13,11 @@
     lang: 'zh-TW',
     
   });
-  console.log("1234");
-  
+  window._genesys.widgets.cobrowse = {
+    src: 'https://apps.mypurecloud.jp/genesys-bootstrap/genesys.min.js'+'/cobrowse/js/gcb.min.js?apikey=55b977a6-5a9a-4548-b2bb-f7485763eb14',
+    url: 'https://apps.mypurecloud.jp/genesys-bootstrap/genesys.min.js'+'/cobrowse/',
+    apikey: '55b977a6-5a9a-4548-b2bb-f7485763eb14',
+    lang: 'zh-TW'
+    };
 
   
