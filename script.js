0797445941a5c014d0a55548daba43cad41d633f
Henry Yen <henryyen0626@gmail.com>
2023-08-23 17:32:23 +0800

    Test

--- script.js
+++ script.js
@@ -7,17 +7,17 @@
     g[e].t = 1 * new Date();
     g[e].c = es;
     ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
-  })(window, 'Genesys', 'https://apps.mypurecloud.jp/genesys-bootstrap/genesys.min.js', {
-    environment: 'prod-apne1',
-    deploymentId: '55b977a6-5a9a-4548-b2bb-f7485763eb14',
-    lang: 'zh-TW',
-    
-  },window._genesys.widgets.cobrowse = {
+  })(window._genesys.widgets.cobrowse = {
     src: 'https://apps.mypurecloud.jp/genesys-bootstrap/genesys.min.js'+'/cobrowse/js/gcb.min.js?apikey=55b977a6-5a9a-4548-b2bb-f7485763eb14',
     url: 'https://apps.mypurecloud.jp/genesys-bootstrap/genesys.min.js'+'/cobrowse/',
     apikey: '55b977a6-5a9a-4548-b2bb-f7485763eb14',
     lang: 'zh-TW'
-    });
+    }, 'Genesys', 'https://apps.mypurecloud.jp/genesys-bootstrap/genesys.min.js', {
+    environment: 'prod-apne1',
+    deploymentId: '55b977a6-5a9a-4548-b2bb-f7485763eb14',
+    lang: 'zh-TW',
+    
+  });
   
 
   
