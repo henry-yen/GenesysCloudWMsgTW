b7b30d5c73175e7d579d2f131fbe2bc533d972d3
Henry Yen <henryyen0626@gmail.com>
2023-08-23 11:22:17 +0800

    create a HTML and JS file

---
+++ WebMsg.html/script.js
@@ -0,0 +1,14 @@
+
+  (function (g, e, n, es, ys) {
+    g['_genesysJs'] = e;
+    g[e] = g[e] || function () {
+      (g[e].q = g[e].q || []).push(arguments)
+    };
+    g[e].t = 1 * new Date();
+    g[e].c = es;
+    ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
+  })(window, 'Genesys', 'https://apps.mypurecloud.jp/genesys-bootstrap/genesys.min.js', {
+    environment: 'prod-apne1',
+    deploymentId: '55b977a6-5a9a-4548-b2bb-f7485763eb14'
+  });
+  console.log("Hello World");
