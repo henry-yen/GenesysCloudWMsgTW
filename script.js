127a68de8287e53746eec3778b853a7d0c225e0e
Henry Yen <henryyen0626@gmail.com>
2023-08-23 18:02:22 +0800

    Test

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 
-  (function (g, e, n, es, ys) {
-    g['_genesysJs'] = e;
-    g[e] = g[e] || function () {
-      (g[e].q = g[e].q || []).push(arguments)
-    };
-    g[e].t = 1 * new Date();
-    g[e].c = es;
-    ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
-  })(window._genesys.widgets.cobrowse = {
-    src: 'https://apps.mypurecloud.jp/genesys-bootstrap/genesys.min.js'+'/cobrowse/js/gcb.min.js?apikey=55b977a6-5a9a-4548-b2bb-f7485763eb14',
-    url: 'https://apps.mypurecloud.jp/genesys-bootstrap/genesys.min.js'+'/cobrowse/',
-    apikey: '55b977a6-5a9a-4548-b2bb-f7485763eb14',
-    lang: 'zh-TW'
-    }, 'Genesys', 'https://apps.mypurecloud.jp/genesys-bootstrap/genesys.min.js', {
-    environment: 'prod-apne1',
-    deploymentId: '55b977a6-5a9a-4548-b2bb-f7485763eb14',
-    lang: 'zh-TW',
-    
-  });
+(function (g, e, n, es, ys) {
+  g['_genesysJs'] = e;
+  g[e] = g[e] || function () {
+    (g[e].q = g[e].q || []).push(arguments)
+  };
+  g[e].t = 1 * new Date();
+  g[e].c = es;
+  ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
+})(window, 'Genesys', 'https://apps.mypurecloud.jp/genesys-bootstrap/genesys.min.js', {
+  environment: 'prod-apne1',
+  deploymentId: '55b977a6-5a9a-4548-b2bb-f7485763eb14'
+});
+ 
   
 
   
