b41ac196e4f6dac3e97aaf87cdd830068093f337
Henry Yen <henryyen0626@gmail.com>
2023-08-23 14:37:15 +0800

    Chinese

--- script.js
+++ script.js
@@ -6,9 +6,9 @@
     };
     g[e].t = 1 * new Date();
     g[e].c = es;
-    ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8'; document.head.appendChild(ys);
+    ys = document.createElement('script'); ys.async = 1; ys.src = n; ys.charset = 'utf-8';ys.lang = 'zh-TW'; document.head.appendChild(ys);
   })(window, 'Genesys', 'https://apps.mypurecloud.jp/genesys-bootstrap/genesys.min.js', {
     environment: 'prod-apne1',
     deploymentId: '55b977a6-5a9a-4548-b2bb-f7485763eb14'
   });
-  console.log("Hello World");
+  
